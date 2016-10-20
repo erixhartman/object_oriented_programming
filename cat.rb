a1fee947fe7642d4b7fb7b4b22d3bb038b9edfa2
Eric Hartman <imerichartman@gmail.com>
2016-10-20 13:53:08 -0400

    Create eats_at instance, not sure how to make AM/PM

--- cat.rb
+++ cat.rb
@@ -1,10 +1,18 @@
 class Cat
+
   def initialize(name, preferred_food, meal_time)
     @name           = name
     @preferred_food = preferred_food
     @meal_time      = meal_time
   end
+
+  def eats_at
+    "#{@name} eats at #{@meal_time}"
+  end
+
 end
 
 jim = Cat.new("Jim", "Pizza", 18)
 pam = Cat.new("Pam", "Cookies", 17)
+
+puts jim.eats_at
