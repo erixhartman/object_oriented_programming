8fff74778d3879fb01d4c95baf085b1e0ec9cfbd
Eric Hartman <imerichartman@gmail.com>
2016-10-20 18:41:43 -0400

    Rover moves properly, no plateau size

--- rover.rb
+++ rover.rb
@@ -1,4 +1,11 @@
 class Rover
+    #
+    # def plateau (l,w)
+    #   puts "Please indicate the length (in units) of the plateau you are on"
+    #   l = gets.chomp.to_i
+    #   puts "Please indicate the width of the plateu"
+    #   w = gets.chomp.to_i
+    # end
 
   def initialize(x, y, direction)
     @x         = x
@@ -6,39 +13,90 @@ class Rover
     @direction = direction
   end
 
-  puts "Do you want to move or turn?"
-  def read_instruction
-    command = gets.chomp.downcase
-
-    if command == "move"
-      move
-    elsif command == "turn"
-      turn
-    end
-
-    def move
-      puts "Do you want to move one space"
-      puts "in the direction you are facing? Y/N"
-      command gets.chomp.upcase
-        if command == "Y"
-          #MOVE FORWARD ONE SPACE
-        elsif command == "N"
-          #BACK TO READ INSTURCTION
-        else puts "Please select Y/N"
-        end
-
-    def turn
-      puts "Which direction would you"
-      puts "like to turn? (L/R)"
-      command = gets.chomp.upcase
-      if command == "L"
-        #Move left 90
-      elsif command == "R"
-        #Move right 90
-      else #BACK TO READ INSTRUCTION
+  def read_instructions(command)
+    command.chars.each do |input|
+      if input == "R"
+        r_turn
+      elsif input == "L"
+        l_turn
+      elsif input == "M"
+        puts "matched command M"
+        move
+      else
+        puts "Error!"
       end
-      else puts "Please select move or turn"
+    end
+    puts "The Rover is at #{@x},#{@y} facing #{@direction}"
+
+  end
+
+  def r_turn
+     if @direction   == "N"
+       @direction = "E"
+    elsif @direction == "E"
+       @direction = "S"
+    elsif @direction == "S"
+       @direction = "W"
+    elsif @direction == "W"
+       @direction = "N"
+    else
+      puts "ERROR"
+    end
+  end
+
+  def l_turn
+     if @direction   == "N"
+       @direction  = "W"
+    elsif @direction == "W"
+       @direction  = "S"
+    elsif @direction == "S"
+       @direction  = "E"
+    elsif @direction == "E"
+       @direction  = "N"
+    else
+      puts "ERROR"
+    end
+  end
+
+  def move
+    puts "Inside method move"
+    if @direction    == "N"
+      puts "matched direction N"
+      @y += 1
+    elsif @direction == "E"
+      puts "matched direction E"
+      @x += 1
+    elsif @direction == "S"
+      puts "matched direction S"
+      @y -= 1
+    elsif @direction == "W"
+      puts "matched direction W"
+      @x -= 1
+    else
+      puts "ERROR"
+    end
+  end
 
 end
 
-eric.Rover(0,0, "E")
+puts "What is your rover's starting position on the X axis"
+x = gets.chomp.to_i
+puts "What is your rover's starting position on the Y axis"
+y = gets.chomp.to_i
+puts "What direction are you facing (N/E/S/W)"
+direction = gets.chomp.upcase
+puts "Your rover is currently at #{x},#{y} and it is facing #{direction}"
+
+puts "Please input the entirety of your rover's movements"
+puts "To turn the rover left input 'L'."
+puts "To turn the rover right input 'R'."
+puts "To move the rover one space in the direction"
+puts "you are facing input 'M'"
+puts "Please input all of your desired instructions"
+puts "then press Enter"
+#For each L turn left, for each R turn right, for each M move 1 space
+instruction = gets.chomp.upcase
+#need to establish size and ensure dont move outside size
+
+new_rover = Rover.new(x, y, direction)
+new_rover.read_instructions(instruction)
