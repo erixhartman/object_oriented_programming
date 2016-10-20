b48b2686402c579cc7faceda3e76f559b7013c97
Eric Hartman <imerichartman@gmail.com>
2016-10-20 13:28:41 -0400

    Create cats Jim & Pam

--- cat.rb
+++ cat.rb
@@ -1,5 +1,10 @@
 class Cat
   def initialize(name, preferred_food, meal_time)
-
+    @name           = name
+    @preferred_food = preferred_food
+    @meal_time      = meal_time
   end
 end
+
+jim = Cat.new("Jim", "Pizza", 18)
+pam = Cat.new("Pam", "Cookies", 17)
