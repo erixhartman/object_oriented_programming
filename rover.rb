6bf30732eff8e84e2ec8263d09f0f8267184802f
Eric Hartman <imerichartman@gmail.com>
2016-10-20 16:44:18 -0400

    First call for help actually saved

--- rover.rb
+++ rover.rb
@@ -8,32 +8,37 @@ class Rover
 
   puts "Do you want to move or turn?"
   def read_instruction
-    gets.chomp
-    if "move"
-      def move
-        puts "Do you want to move one space"
-        puts "in the direction you are facing? Y/N"
-        gets.chomp.upcase
-          if "Y"
-            #MOVE FORWARD ONE SPACE
-          elsif "N"
-            #BACK TO READ INSTURCTION
-          else puts "Please select Y/N"
-          end
-      end
-    elsif "turn"
-      def turn
-        puts "Which direction would you"
-        puts "like to turn? (L/R)"
-        gets.chomp.upcase
-        if "L"
-          #Move left 90
-        elsif "R"
-          #Move right 90
-        else #BACK TO READ INSTRUCTION
-        end
-      end
-    else puts "Please select move or turn"
+    command = gets.chomp.downcase
+
+    if command == "move"
+      move
+    elsif command == "turn"
+      turn
     end
 
+    def move
+      puts "Do you want to move one space"
+      puts "in the direction you are facing? Y/N"
+      command gets.chomp.upcase
+        if command == "Y"
+          #MOVE FORWARD ONE SPACE
+        elsif command == "N"
+          #BACK TO READ INSTURCTION
+        else puts "Please select Y/N"
+        end
+
+    def turn
+      puts "Which direction would you"
+      puts "like to turn? (L/R)"
+      command = gets.chomp.upcase
+      if command == "L"
+        #Move left 90
+      elsif command == "R"
+        #Move right 90
+      else #BACK TO READ INSTRUCTION
+      end
+      else puts "Please select move or turn"
+
 end
+
+eric.Rover(0,0, "E")
